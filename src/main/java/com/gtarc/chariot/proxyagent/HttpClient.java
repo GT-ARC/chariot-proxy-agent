431f1e4ea0025c6077ca9f38f18595d5c04acaa9
Frederic <Frederic.Abraham1996@googlemail.com>
2019-10-02 15:40:22 +0200

    Added http client to receive device to agent map from the kms

---
+++ src/main/java/com/gtarc/chariot/proxyagent/HttpClient.java
@@ -0,0 +1,52 @@
+package com.gtarc.chariot.proxyagent;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class HttpClient {
+    private static final String startUrl = "http://chariot-km.dai-lab.de:8001/monitoringservice/";
+    private static final String postfix = "?format=json";
+    private static String mappingsURL = "";
+    private OkHttpClient client = new OkHttpClient();
+
+    public HashMap<String, String> establishConnection() {
+        HashMap<String, String> retMap = new HashMap<>();
+        Request request = new Request.Builder()
+                .url(startUrl + postfix)
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            JSONParser parser = new JSONParser();
+
+            String body = response.body().string();
+            if (response.code() != 404 && !body.equals("[]")) {
+
+                Object receivedO = parser.parse(body);
+
+                JSONObject monService;
+                if (receivedO instanceof JSONArray) monService = ((JSONObject) ((JSONArray) receivedO).get(0));
+                else monService = ((JSONObject) receivedO);
+
+                JSONObject mapping = ((JSONObject) monService.get("agentlist"));
+                mappingsURL = (String) mapping.get("url");
+                JSONArray jsonArray = (JSONArray) mapping.get("mappings");
+                jsonArray.forEach(o -> {
+                    JSONObject element = ((JSONObject) o);
+                    retMap.put((String) element.get("device_id"), (String) element.get("agent_id"));
+                });
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return retMap;
+    }
+}
--- src/main/java/com/gtarc/chariot/proxyagent/ProxyAgent.java
+++ src/main/java/com/gtarc/chariot/proxyagent/ProxyAgent.java
@@ -1,21 +1,40 @@
 package com.gtarc.chariot.proxyagent;
 
 import de.dailab.jiactng.agentcore.SimpleAgentNode;
+import de.dailab.jiactng.agentcore.action.Action;
 import de.dailab.jiactng.agentcore.action.scope.ActionScope;
 import de.dailab.jiactng.agentcore.lifecycle.LifecycleException;
+import de.dailab.jiactng.agentcore.ontology.IActionDescription;
 import de.dailab.jiactng.rsga.beans.AbstractRESTfulAgentBean;
 import org.springframework.context.ApplicationContext;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
-public class ProxyAgent extends AbstractRESTfulAgentBean
-{
+public class ProxyAgent extends AbstractRESTfulAgentBean {
+
+    private HttpClient httpClient = new HttpClient();
+    private HashMap<String, String> deviceIDToAgentID = new HashMap<>();
+    private HashMap<String, IActionDescription> cachedActions = new HashMap<>();
+
+    private static final String PROPERTY_ACTION = "com.gtarc.chariot.receive_property_action";
+
+    @Override
+    public void doStart() throws Exception {
+        this.deviceIDToAgentID = httpClient.establishConnection();
+    }
 
     /**
      * Example get request.
      * Invoke with localhost:8080/chariot/ProxyAgent/id?id=test&testParam=1
-     * @param id example param
+     *
+     * @param id        example param
      * @param testParam example extra param must be from type int
      * @return a combination of both
      */
@@ -29,6 +48,7 @@ public class ProxyAgent extends AbstractRESTfulAgentBean
     /**
      * Example action for a post request in wich the parameters a send via
      * json in the request body. The header Content-Type application/json must be set.
+     *
      * @param x first parameter
      * @param y second parameter
      * @return the sum of both parameters
@@ -36,17 +56,58 @@ public class ProxyAgent extends AbstractRESTfulAgentBean
     @POST
     @Path("/add")
     @Expose(scope = ActionScope.WEBSERVICE)
-    public int add( @QueryParam("x") int x ,
-                    @QueryParam("y") int y) {
+    public int add(@QueryParam("x") int x,
+                   @QueryParam("y") int y) {
         return x + y;
     }
 
+    @POST
+    @Path("sendAction")
+    @Expose(scope = ActionScope.WEBSERVICE)
+    public void relayPropertyDelegation(@QueryParam("device_id") String deviceID,
+                                        @QueryParam("value") String jsonObject) {
+
+        // Check if device id is in agent list
+        String agentID = this.deviceIDToAgentID.get(deviceID);
+        if (agentID == null) {
+            // Query new updates
+            this.deviceIDToAgentID = httpClient.establishConnection();
+
+            agentID = this.deviceIDToAgentID.get(deviceID);
+            if (agentID == null) {
+                System.err.println("Agent not found for device: " + deviceID);
+                return;
+            }
+        }
+
+        // Receive the cached action if no action is cached search for new one
+        IActionDescription cachedDesc = this.cachedActions.get(agentID);
+        if (cachedDesc == null) {
+            List<IActionDescription> actionDescriptions = thisAgent.searchAllActions(new Action(PROPERTY_ACTION));
+            System.out.println(Arrays.toString(actionDescriptions.toArray()));
+            String finalAgentID = agentID;
+            Optional<IActionDescription> optionalDesc = actionDescriptions.stream().filter(
+                    desc -> desc.getProviderDescription().getAid().equals(finalAgentID)).findAny();
+            if (optionalDesc.isPresent())
+                cachedDesc = optionalDesc.get();
+            else {
+                System.err.println("No action description found for device agent: " + agentID + " of device " + deviceID);
+                return;
+            }
+            this.cachedActions.put(agentID, cachedDesc);
+        }
+
+        // Invoke the agent
+        invoke(cachedDesc, new String[]{jsonObject});
+    }
+
+
     /**
      * Start the Proxy Agent Node
+     *
      * @param args main args
      */
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         ApplicationContext context = SimpleAgentNode.startAgentNode("classpath:Proxy_Agent.xml", "jiactng_log4j.properties");
         SimpleAgentNode node = (SimpleAgentNode) context.getBean("ProxyAgentNode");
         try {
