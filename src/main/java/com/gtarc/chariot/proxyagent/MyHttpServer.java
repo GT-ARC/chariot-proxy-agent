2d72e67e82cd45e4f06e5997a249cfebd101f5ae
Frederic <Frederic.Abraham1996@googlemail.com>
2019-10-09 16:47:43 +0200

    Fixed the proxy agent http implementation

--- src/main/java/com/gtarc/chariot/proxyagent/MyHttpServer.java
+++ src/main/java/com/gtarc/chariot/proxyagent/MyHttpServer.java
@@ -1,6 +1,7 @@
 package com.gtarc.chariot.proxyagent;
 
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpServer;
 import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONObject;
@@ -14,6 +15,7 @@ import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
 
 public class MyHttpServer {
 
@@ -26,6 +28,7 @@ public class MyHttpServer {
         {
             HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
             createServerContext(server);
+            server.setExecutor(Executors.newCachedThreadPool());
             server.start();
         }
         catch (Throwable tr)
@@ -46,24 +49,40 @@ public class MyHttpServer {
     private void sendResponse(HttpExchange httpExchange, int code, String message, String contentType) throws IOException {
         byte[] response = message.getBytes(StandardCharsets.UTF_8);
 
-        httpExchange.getResponseHeaders().add("Content-Type", contentType);
-        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        Headers headers = httpExchange.getResponseHeaders();
+        headers.add("Access-Control-Allow-Headers","x-prototype-version,x-requested-with,Content-Type,Authorization");
+        headers.add("Access-Control-Allow-Methods","GET,POST");
+        headers.add("Access-Control-Allow-Origin","*");
+        headers.add("Content-Type", contentType);
+
+        if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+            httpExchange.sendResponseHeaders(204, -1);
+            return;
+        }
+
         httpExchange.sendResponseHeaders(code, response.length);
 
         OutputStream out = httpExchange.getResponseBody();
         out.write(response);
         out.close();
+        httpExchange.close();
     }
 
     private void receiveSendRequest(HttpExchange httpExchange) throws IOException {
+        System.out.println("Request: " + Thread.currentThread().getName());
         String body = "";
         try {
             StringWriter writer = new StringWriter();
             IOUtils.copy(httpExchange.getRequestBody(), writer, Charset.defaultCharset());
             body = writer.toString();
         } catch (IOException e) {
+            System.err.println("Massage Parse Excepiton");
             sendResponse(httpExchange, 400, "Message Parse Exception", "text/plain; charset=UTF-8");
         }
+
+        System.out.println(body);
+        if (httpExchange.getRequestMethod().equalsIgnoreCase("OPTIONS"))
+            sendResponse(httpExchange, 200, "","");
 
         JSONParser parser = new JSONParser();
         String device_id = "";
@@ -81,6 +100,7 @@ public class MyHttpServer {
         } catch (Exception e) {
             sendResponse(httpExchange, 404, e.getMessage(), "text/plain; charset=UTF-8");
         }
+        sendResponse(httpExchange, 200, "Allet ok", "text/plain; charset=UTF-8");
     }
 
 }
